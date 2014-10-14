f610c8d7af8710e55260791ea2edc866460ec1b6
Tamara Will <tamara@tamarawill.com>
2014-10-14 17:41:49 -0400

    Added some testing routes to routes.php

--- app/routes.php
+++ app/routes.php
@@ -13,5 +13,50 @@
 
 Route::get('/', function()
 {
-	return View::make('hello');
+	return 'Welcome to the home page!';
+});
+
+
+Route::get('/visit', function()
+{
+	return 'We are open 24 hours a day, 7 days a week! Our phone number is 617-555-1212';
+});
+
+Route::get('/pastries', function()
+{
+	return 'We have a large selection of delicious pastries.';
+});
+
+Route::get('/pastries/{category}', function($category) {
+        return 'Here are all the pastries in the category of '.$category.'.';
+}); 
+
+Route::get('/beverages/coffee', function()
+{
+	return 'Our coffee is all fair trade, and generally awesome.';
+});
+
+Route::get('/beverages/tea', function()
+{
+	return 'Our teas are estate grown, and super fabulous.';
+});
+
+Route::get('/talktous', function() {
+
+    $view  = '<form method="POST">';
+    $view .= 'What\'s on your mind? <input type="text" name="whatsup">';
+    $view .= '<input type="submit">';
+    $view .= '</form>';
+    return $view;
+
+});
+Route::post('/talktous', function() {
+
+    $input =  Input::all();
+    print_r($input);
+
+});
+
+Route::get('/practice', function() {
+    echo App::environment();
 });
